65cb10bd03d9ebdb8dc4ff41b6e04127f3697673
Shanshine <salsabilshanaa0911@gmail.com>
2026-06-03 18:33:21 +0700

    fix whitenoise storage

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,12 +44,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",  
     },
 }
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'  
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
