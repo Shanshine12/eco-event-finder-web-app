1e2f1e8dd3010570e36ba7da47720502d94d3e7c
Shanshine <salsabilshanaa0911@gmail.com>
2026-06-03 17:29:01 +0700

    add cloudinary for media storage

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,17 @@ INSTALLED_APPS = [
     'bookmarks',
     'rest_framework.authtoken',
     'accounts',
+    'cloudinary_storage',
+    'cloudinary',
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
